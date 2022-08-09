4f88a6fdcc54aed4afdb42910731839ebdbdbbee
VitorMM <vitor251093@gmail.com>
2022-08-09 02:42:32 -0300

    Starting to add support to CrossOver and Proton

--- build.sh
+++ build.sh
@@ -4,8 +4,14 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 DIR="$(realpath "${DIR}")"
 cd "$DIR"
 
-version="${1:-stable}"
-build="${2:-6.0.4}"
+if [$# -le 2]; then 
+    echo "illegal number of parameters"
+    exit 1
+fi
+
+version="${1}"
+build="${2}"
+winebuild="${build}"
 
 distro="debian"
 distro_version="buster"
@@ -24,9 +30,17 @@ fi
 if [ "$version" = "staging" ]; then
     declare -A info=( ["base"]="official"  ["package"]="winehq-staging"  ["readableName"]="Staging" )
 fi
+if [ "$version" = "crossover" ]; then
+    declare -A info=( ["base"]="custom"    ["package"]="winehq-${3}"     ["readableName"]="CX" )
+    winebuild="${4:-stable}"
+fi
+if [ "$version" = "proton" ]; then
+    declare -A info=( ["base"]="proton"    ["package"]="winehq-${3}"     ["readableName"]="Proton" )
+    winebuild="${4:-7.14}"
+fi
 
 baseFilePath="base_wine_${info["base"]}.yml"
-wineVersion="${info["package"]}=${build}~${distro_version}"
+wineVersion="${info["package"]}=${winebuild}~${distro_version}"
 appimageVersion="AI1Wine${info["readableName"]}64Bit${build}"
 
 # TODO: Crossover support is still in development
@@ -39,10 +53,24 @@ if [ ! -f "$fullPkg2appimagePath" ]; then
     exit 1
 fi
 
-# Adding package URL
+# Loading base file
 dataYaml=$(cat $baseFilePath)
 
+if [ "$version" = "crossover" ]; then
+    sourceUrl="https://media.codeweavers.com/pub/crossover/source/crossover-sources-${build}.tar.gz"
+    wget $sourceUrl -O crossover.tar.gz
+    tar -xf crossover.tar.gz -C .
+    cd sources/wine
+    ./configure CC="clang" CXX="clang++" --enable-win64 --disable-winedbg --without-x --without-vulkan --disable-mscms # requires clang flex bison libfreetype6-dev
+    exit 0
+fi
+if [ "$version" = "proton" ]; then
+    urlBuild="${build//./-}"
+    dataYaml="${dataYaml//__proton_build__/"$urlBuild"}"
+fi
+
 # Adding base file variables
+dataYaml="${dataYaml//__build__/"$build"}"
 dataYaml="${dataYaml//__version__/"$version"}"
 dataYaml="${dataYaml//__distro__/"$distro"}"
 dataYaml="${dataYaml//__distro_version__/"$distro_version"}"
